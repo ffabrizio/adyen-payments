bb82d362c4f21ba1a074bb3adc899f76703ce248
Fabio Fabrizio <ffabrizio@gmail.com>
2018-07-21 09:40:54 +0100

    HTML example

--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ let payments = loadPayments(data, labels);
 
 window.payments = payments;
 
+/*
 console.log(`Loaded ${ payments.length } payments`);
+console.log(payments); 
+let selectedPayment = payments[0]; 
+console.log('selection', selectedPayment); 
+console.log('schema', selectedPayment.schema); 
+selectedPayment.setValue( { 'cardDetails.cvc' : 'ABCD'} ); 
+console.log('value', selectedPayment.getValue()); 
+console.log('schema', selectedPayment.schema);
+*/
