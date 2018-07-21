3a6460ba62258516d29ec295874ac79b85a2984b
Fabio Fabrizio <ffabrizio@gmail.com>
2018-07-21 09:27:17 +0100

    helpers for setting and getting values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import {loadPayments} from './payments';
-import data from './data.json';
+import { loadPayments } from './payments';
+import data from './sampledata.json';
 
-const labels = {
-    "cardDetails.cvc" : "Security Code"
-};
+const labels = { "cardDetails.cvc" : "Security Code" };
 
-window.adyenPayments = loadPayments(data, labels);
+let payments = loadPayments(data, labels);
+
+window.payments = payments;
+
+console.log(`Loaded ${ payments.length } payments`);
